4c215409c8265190169d48a3240b570b5f8b5512
Kevin Davis <Kevindavis5454@gmail.com>
2020-02-01 17:53:48 -0700

    added accordian

--- src/App.js
+++ src/App.js
@@ -1,46 +1,22 @@
-import React from 'react';
-import Split from './composition/Split';
-import './App.css';
-import Tooltip from './composition/Tooltip';
-import Messages from './Messages';
-import TheDate from './state/TheDate';
-import Counter from './state/Counter';
-import HelloWorld from "./state-drills/HelloWorld";
-import Bomb from "./state-drills/Bomb";
-import RouletteGun from "./state-drills/RouletteGun";
+import React from "react";
+import Tabs from './state/Tabs';
 
-const firstTooltip = (
-    <Tooltip color='hotpink' message = 'tooltip message'>
-        ipsum
-    </Tooltip>
-)
-const secondTooltip = (
-    <Tooltip color='#126BCC' message = 'another tooltip message'>
-        officiis
-    </Tooltip>
-)
+// array of objects each with a name and content
+const tabsProp = [
+    { name: 'First tab',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+    { name: 'Second tab',
+        content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+    { name: 'Third tab',
+        content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+];
 
-function App() {
-  return (
-      <main className='App'>
-        <HelloWorld />
-        <Bomb />
-        <RouletteGun />
-       {/*   <Counter count={123} />
-          <TheDate />
-          <Messages name='Messages' unread={0} />
-          <Messages name='Notifications' unread={10} />
-        <Split className='left' flexBasis={2}>
-            This is the content for the left `Split`. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?
-        <Tooltip message = 'one more tooltip message'>
-            Necess
-        </Tooltip>
-        </Split>
-          <Split className='right'>
-              This is the content for the right `Split`. Inventore aliquid cupiditate suscipit repellat. Quaerat quis officiis quam fuga. Aliquid quo possimus id soluta aspernatur.
-          </Split>*/}
-      </main>
-  );
+class App extends React.Component {
+    render() {
+        return <div>
+            <Tabs tabs={tabsProp} />
+        </div>
+    }
 }
 
 export default App;
